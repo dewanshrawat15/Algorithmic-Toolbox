929a9715c8091f948d97fe6d49b69b3981bb20cd
Dewansh Rawat <dewanshrawat15@gmail.com>
2021-06-12 08:45:03 +0530

    Solving dp1 questions

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,40 @@
 # Uses python3
-import sys
 
-def get_change(m):
+def get_change_rec_dp(m, dp):
     #write your code here
-    return m // 4
+    if m == 0:
+        return 1
+    ways = []
+    if m - 4 >= 0:
+        if dp[m - 4] == -1:
+            dp[m - 4] = get_change_rec_dp(m - 4, dp)
+        ways.append(dp[m - 4])
+    if m - 3 >= 0:
+        if dp[m - 3] == -1:
+            dp[m - 3] = get_change_rec_dp(m - 3, dp)
+        ways.append(dp[m - 3])
+    if m - 1 >= 0:
+        if dp[m - 1] == -1:
+            dp[m - 1] = get_change_rec_dp(m - 1, dp)
+        ways.append(dp[m - 1])
+    return min(ways) + 1
+
+def get_change_it(m):
+    dp = [0] + [10**9] * m
+    dp[1] = 1
+    dp[2] = 2
+    denominations = [1, 3, 4]
+    for i in range(3, m + 1):
+        for j in denominations:
+            if i >= j:
+                coins = dp[i - j] + 1
+                if coins < dp[i]:
+                    dp[i] = coins
+    return dp[-1]
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    m = int(input())
+    dp = [-1 for i in range(m)]
+    print(get_change_rec_dp(m, dp) - 1)
+    print(get_change_it(m))
